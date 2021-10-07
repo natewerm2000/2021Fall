0439b8b0263e6a4de874c6f0d155a2d21c956ce4
natewerm2000 <alexandn7@newpaltz.edu>
2021-10-07 13:52:56 -0400

    implemented vue

--- client/components/nav.vue
+++ client/components/nav.vue
@@ -3,17 +3,18 @@
     <nav class="navbar" role="navigation" aria-label="main navigation">
   <div class="navbar-brand">
     <a class="navbar-item" href="https://bulma.io">
-      <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
+      <img src="../assets/logo.png" width="112" height="28">
+      Be Happy
     </a>
 
-    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" :class = "{'is-active': isActive}" @click="isActive = !isActive">
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
     </a>
   </div>
 
-  <div id="navbarBasicExample" class="navbar-menu">
+  <div class="navbar-menu" :class = "{'is-active': isActive}">
     <div class="navbar-start">
       <a class="navbar-item"href: home>
         Home
@@ -72,11 +73,17 @@
 </style>
 
 <script>
+import LoginBadge from '.\LoginBadge'
 
-import {Nav} form "./componenets/Nav";
 export default {
-  components: {
-    Nav
+  data(){
+      return{
+          isActive: false
+      }
+  },
+    components: {
+        LoginBadge
+    }
   }
   
 }
---
+++ client/src/components/loginBadge.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="buttons">
+          <a class="button is-primary">
+            <strong>Sign up</strong>
+          </a>
+          <a class="button is-light">
+            Log in
+          </a>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
